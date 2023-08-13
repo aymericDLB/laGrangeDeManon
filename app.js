3d78594824ac4e3a1c64ec3f1988c85b1ab2e194
MricMric <aymeric.delabarre@gmail.com>
2023-08-13 16:25:59 +0200

    erer

--- app.js
+++ app.js
@@ -1,52 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path')
-const http = require('http');
+var http = require('http');
 
 
-// Définition des Routes
-const userRoutes = require('./routes/userRoutes');
-const reservationRoutes = require('./routes/reservationRoutes');
-const messageRoutes = require('./routes/messageRoutes');
-const loginRoutes = require('./routes/loginRoutes');
-const clientRoutes = require('./routes/clientRoutes');
-const transverseRoutes = require('./routes/transverseRoutes');
-const planningRoutes = require('./routes/planningRoutes');
-const mailRoutes = require('./routes/mailRoutes');
-
-const app = express();
-
-// Middleware pour le parsing du corps des requêtes en JSON
-app.use(express.json());
-
-// Utilisations des Routes
-app.use(userRoutes);
-app.use(reservationRoutes);
-app.use(messageRoutes);
-app.use(loginRoutes);
-app.use(clientRoutes);
-app.use(transverseRoutes);
-app.use(planningRoutes);
-app.use(mailRoutes);
-
-app.use(cors({origin: '*'}));
-//app.use(express.static('public/frontend'));
-
-
-app.use(express.static(path.join(__dirname, '/dist')));
-
-// Configurer la redirection vers index.html pour toutes les routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello world!');
 });
+server.listen(process.env.PORT);
 
-module.exports = app;
-// Démarrer le serveur
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`Serveur Node.js en cours d'exécution sur le port ${port}`);
-// });
-
-// app.listen(port, () => {
-//   console.log(`Serveur démarré sur le port ${port}`);
-// });
+console.log('The server is available at http://127.0.0.1:' + process.env.PORT + '/');
