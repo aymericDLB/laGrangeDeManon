2a5fb45c842e42893a0428eb5c28c9c3d540db2d
MricMric <aymeric.delabarre@gmail.com>
2023-12-10 08:57:19 +0100

    securisation basic

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path')
 const http = require('http');
-
+const basicAuth = require('basic-auth');
 
 // Définition des Routes
 //const userRoutes = require('./routes/userRoutes');
@@ -16,11 +16,32 @@ const mailRoutes = require('./routes/mailRoutes');
 
 const app = express();
 
+const authenticate = (req, res, next) => {
+  const user = basicAuth(req);
+
+  // Vérifier si les informations d'identification sont présentes et correctes
+  if ((!user || !checkCredentials(user.name, user.pass)) && req.url.includes("/api")) {
+    res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return res.sendStatus(401);
+  }
+
+  // Informations d'identification valides, passez à la suite
+  next();
+};
+
+// Fonction de vérification des informations d'identification
+const checkCredentials = (username, password) => {
+  // Remplacez cela par la logique réelle de vérification des identifiants
+  return username === 'Lagrangedemanon' && password === '$2y$10$LL5m3nVMUSsPYwJvf4tm/Oktsxi377mslkTd/HymkY6WamK0xjI9y';
+};
+
+// Utilisez le middleware d'authentification basique
+app.use(authenticate);
+
 // Middleware pour le parsing du corps des requêtes en JSON
 app.use(express.json());
 
 // Utilisations des Routes
-//app.use(userRoutes);
 app.use(reservationRoutes);
 app.use(messageRoutes);
 app.use(loginRoutes);
