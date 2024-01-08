4794ae4ec1568fdb03643a467c75e2070f3ca986
MricMric <aymeric.delabarre@gmail.com>
2024-01-08 14:50:20 +0100

    test

--- utils/database.js
+++ utils/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: 'mysql'
+  dialect: 'mariadb'
 });
 
 try {
